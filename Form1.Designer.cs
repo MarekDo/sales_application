54be97fe0edc9346a3e37f507e450a81f91451b1
Marek <vat77@tlen.pl>
2022-01-24 19:13:13 +0100

    Added buttons 2 to 5 inside panel

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,12 +34,19 @@ namespace sales_application
             this.panelDesktop = new System.Windows.Forms.Panel();
             this.panelLogo = new System.Windows.Forms.Panel();
             this.btn_1 = new System.Windows.Forms.Button();
+            this.panelForButton = new System.Windows.Forms.Panel();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
             this.panelMain.SuspendLayout();
+            this.panelForButton.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMain
             // 
             this.panelMain.BackColor = System.Drawing.Color.Silver;
+            this.panelMain.Controls.Add(this.panelForButton);
             this.panelMain.Controls.Add(this.btn_1);
             this.panelMain.Controls.Add(this.panelLogo);
             this.panelMain.Dock = System.Windows.Forms.DockStyle.Left;
@@ -85,6 +92,58 @@ namespace sales_application
             this.btn_1.Text = "Przycisk_1";
             this.btn_1.UseVisualStyleBackColor = true;
             // 
+            // panelForButton
+            // 
+            this.panelForButton.Controls.Add(this.btn_5);
+            this.panelForButton.Controls.Add(this.btn_4);
+            this.panelForButton.Controls.Add(this.btn_3);
+            this.panelForButton.Controls.Add(this.btn_2);
+            this.panelForButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelForButton.Location = new System.Drawing.Point(0, 134);
+            this.panelForButton.Name = "panelForButton";
+            this.panelForButton.Size = new System.Drawing.Size(153, 94);
+            this.panelForButton.TabIndex = 2;
+            // 
+            // btn_2
+            // 
+            this.btn_2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_2.Location = new System.Drawing.Point(0, 0);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(153, 23);
+            this.btn_2.TabIndex = 0;
+            this.btn_2.Text = "Przycisk_2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            // 
+            // btn_3
+            // 
+            this.btn_3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_3.Location = new System.Drawing.Point(0, 23);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(153, 23);
+            this.btn_3.TabIndex = 1;
+            this.btn_3.Text = "Przycisk_3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            // 
+            // btn_4
+            // 
+            this.btn_4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_4.Location = new System.Drawing.Point(0, 46);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(153, 23);
+            this.btn_4.TabIndex = 2;
+            this.btn_4.Text = "Przycisk_4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            // 
+            // btn_5
+            // 
+            this.btn_5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_5.Location = new System.Drawing.Point(0, 69);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(153, 23);
+            this.btn_5.TabIndex = 3;
+            this.btn_5.Text = "Przycisk_5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -96,6 +155,7 @@ namespace sales_application
             this.Name = "Form1";
             this.Text = "Sales App";
             this.panelMain.ResumeLayout(false);
+            this.panelForButton.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -107,6 +167,11 @@ namespace sales_application
         private System.Windows.Forms.Panel panelDesktop;
         private System.Windows.Forms.Panel panelLogo;
         private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Panel panelForButton;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_2;
     }
 }
 
